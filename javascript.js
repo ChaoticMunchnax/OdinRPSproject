a604bf5aae6597d99bc2241a0449b662e961afd8
Jaden <Jadenmtodd@gmail.com>
2022-10-30 22:21:17 -0400

    Properly Working Game (No GUI)
    Rebuilt the entire program:
    - New random generator for Rock Paper Scissors selection
    - Cleaner and easier to read and understand code
    - Comments added with more detail
    
    Fix for the last version... Realized that the IsZero function was acting
    strangely and my half asleep brain decided the best method of fixing the
     issues is deleting the file and going from there . This is an actually
     working fixed and cleaner looking version.
    - An awfully tired Munchnax...

--- javascript.js
+++ javascript.js
@@ -1,64 +1,42 @@
-function isZero(){
-    let randomNumber = (Math.floor(Math.random() * 10));
+function getComputerChoice(){
+    let paper = 0;
+    let rock = 2;
+    let scissors = 1;
+    //generate a random number between 0 and 1 Mutiply by 3 and round it
+    let randomNum = Math.floor(Math.random() * 3)
     
-    if (randomNumber === 0){
-        return randomNumber = Math.floor(Math.random() * 10)
-    }
-    else{
-        return randomNumber
-    }
+    // checks what type the number is and returns it in string format
+    return randomNum == paper ? 'paper' : randomNum == rock ? 'rock' : randomNum == scissors ? 'scissors' : "Broken, getComputerChoice"
 }
-function round(){
-    if (scissors && playerChoice == "paper"){
-        loss += 1
-        return `You lost scissors beats ${playerChoice}`;
-    }
-    else if ( paper && playerChoice == "rock" ){
-        loss += 1
-        return `You lost paper beats ${playerChoice}`;
-    }
-    else if (rock && playerChoice == "scissors"){
-        loss += 1
-        return `You lost rock beats ${playerChoice}`;
-    }
-    else if (scissors && playerChoice =="rock"){
-        win += 1
-        return `You won! ${playerChoice} beats scissors`
-    } 
-    else if (paper && playerChoice == "scissors"){
-        win += 1
-        return `You won! ${playerChoice} beats paper`
-    }  
-    else if(rock && playerChoice == "paper") {
-        win += 1
-     return `You won! ${playerChoice} beats rock`;
-    }
-    else{
-        tie += 1;
-        return "Tie!"
-    }
+function oneRound(getComputerChoice, playerSelection){
+    // console.log("computer " + getComputerChoice)
+    // console.log("player " + playerSelection)
+    // playerSelection == playerSelection.toLowerCase;
+    // Rock Paper Scissors User vs Bot comparisons
+   
+    return playerSelection == 'paper' && getComputerChoice == 'paper' ? ("You Tied with the Machine..."  )
+    : playerSelection == 'paper' && getComputerChoice == 'rock' ? ("You Win! Paper beats rock")
+    : playerSelection == 'paper' && getComputerChoice == 'scissors' ? ("You lost... Scissors beat paper")
+    // Rock comparisons
+    :  playerSelection == 'rock' && getComputerChoice == 'rock' ? ("You Tied with the Machine..." )
+    : playerSelection == 'rock' && getComputerChoice == 'scissors' ? ("You Win! Rock beats scissors")
+    : playerSelection == 'rock' && getComputerChoice == 'paper' ? ("You lost... Paper beats rock")
+    // Scissors Comparisons
+    :  playerSelection == 'scissors' && getComputerChoice == 'paper' ? ("You Tied with the Machine..." )
+    : playerSelection == 'scissors' && getComputerChoice == 'paper' ? ("You Win! Scissors beats paper")
+    : playerSelection == 'scissors' && getComputerChoice == 'rock' ? ("You lost... Rocks beat scissors")
+    : "Broken, oneRound()"
 }
-
-function game(){
-    for (let i = 0; i < 6; i++){
-        console.log(round())
-        console.log(botChoice)
-        // playerChoice = prompt('Please type rock, paper, or scissors').toLowerCase();
-        console.log(`Round ${iRound = i + 1}`)
+    function game(){
+        for (let i = 0; i < 5; i++){
+            console.log(oneRound(getComputerChoice(),playerSelection))
+        }
     }
-    console.log(`You won ${win} times. You lost ${loss}. You tied ${tie})`);
+    let playerSelection = prompt("Type rock paper scissors");
+    // let Win = 0;
+    // let Loss = 0;
+    // let Tie = 0;
+    console.log(game())
+    // console.log(`You Won ${Win} times!\n You Tied ${Tie} times!\n You lost ${Loss} times....`)
+// console.log(oneRound(getComputerChoice(),playerSelection))
 
-}
-// let playerChoice = prompt('Please type rock, paper, or scissors').toLowerCase();
-let playerChoice = 'paper'
-let tie = 0;
-let win = 0;
-let loss = 0;
-let iRound = 0;
-let botChoice = isZero();
-
-const paper = botChoice > 3 && botChoice < 7;
-const rock = botChoice > 0 && botChoice < 4;
-const scissors = botChoice > 7 && botChoice < 10;
-
-game()
